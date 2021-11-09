eb4de7661fd16d85767887453e9bc3e47c4fac44
Tobi Oyeleye <krisella74@gmail.com>
2021-11-09 23:01:47 +0100

    add some 404 state

--- client/components/song-details.js
+++ client/components/song-details.js
@@ -19,33 +19,43 @@ class SongDetails extends React.Component {
   }
 
   render() {
-    let { song } = this.props.data;
+    let { song, loading } = this.props.data;
     const songId = this.props.params.id;
     return (
       <section>
-        {song && (
-          <div>
-            <header className="song-detail-header">
-              <Link to="/" className="song-detail-back-btn">
-                Back
-              </Link>
-              <h3 className="song-detail-title">{song.title}</h3>
-            </header>
-            <ul className="collection song-lyric-list">
-              {song.lyrics &&
-                song.lyrics.map((lyric) => (
-                  <li key={lyric.id} className="collection-item">
-                    <p>{lyric.content}</p>
-                    <button onClick={() => this.likeLyric(lyric.id)}>
-                      {lyric.likes} {lyric.likes < 2 ? 'like' : 'likes'}
-                    </button>
-                  </li>
-                ))}
-            </ul>
+        {!loading &&
+          (song ? (
+            <div>
+              <header className="song-detail-header">
+                <Link to="/" className="song-detail-back-btn">
+                  Back
+                </Link>
+                <h3 className="song-detail-title">{song.title}</h3>
+              </header>
+              <ul className="collection song-lyric-list">
+                {song.lyrics &&
+                  song.lyrics.map((lyric) => (
+                    <li key={lyric.id} className="collection-item">
+                      <p>{lyric.content}</p>
+                      <button onClick={() => this.likeLyric(lyric.id)}>
+                        {lyric.likes} {lyric.likes < 2 ? "like" : "likes"}
+                      </button>
+                    </li>
+                  ))}
+              </ul>
 
-            <CreateSongLyric songId={songId} />
-          </div>
-        )}
+              <CreateSongLyric songId={songId} />
+            </div>
+          ) : (
+            <div
+              style={{
+                textAlign: "center",
+              }}
+            >
+              <p>sorry the song you are looking for does not exist anymore</p>
+              <Link to="/">Go back home</Link>
+            </div>
+          ))}
       </section>
     );
   }
